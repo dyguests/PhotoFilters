4b1540eb0e1dd177be24c332e3731fdab5e48f44
fanhl <dyguests@qq.com>
2019-03-20 22:46:42 +0800

    hold

--- library/src/main/cpp/native-lib.cpp
+++ library/src/main/cpp/native-lib.cpp
@@ -8,6 +8,8 @@
 //#define  LOGI(...)  __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
 #define  LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
 
+void bitmap_hold_pixels(const JNIEnv *env, const _jobject *bitmap);
+
 extern "C" JNIEXPORT void JNICALL Java_com_fanhl_photofilters_PhotoFilterApi_brightness(JNIEnv *env, jclass obj, jobject bitmap, jfloat brightnessValue) {
     AndroidBitmapInfo info;
     int ret;
@@ -26,6 +28,28 @@ extern "C" JNIEXPORT void JNICALL Java_com_fanhl_photofilters_PhotoFilterApi_bri
     }
 
     brightness(&info, pixels, brightnessValue);
+
+    AndroidBitmap_unlockPixels(env, bitmap);
+}
+
+void bitmap_hold_pixels(JNIEnv *env, jobject bitmap) {
+    AndroidBitmapInfo info;
+    int ret;
+    void *pixels;
+
+    if ((ret = AndroidBitmap_getInfo(env, bitmap, &info)) < 0) {
+        LOGE("AndroidBitmap_getInfo() failed ! error=%d", ret);
+        return;
+    }
+    if (info.format != ANDROID_BITMAP_FORMAT_RGBA_8888) {
+        LOGE("Bitmap format is not RGBA_8888 !");
+        return;
+    }
+    if ((ret = AndroidBitmap_lockPixels(env, bitmap, &pixels)) < 0) {
+        LOGE("AndroidBitmap_lockPixels() failed ! error=%d", ret);
+    }
+
+    gray(&info, pixels);
 
     AndroidBitmap_unlockPixels(env, bitmap);
 }
@@ -52,27 +76,8 @@ extern "C" JNIEXPORT void JNICALL Java_com_fanhl_photofilters_PhotoFilterApi_inv
     AndroidBitmap_unlockPixels(env, bitmap);
 }
 
-extern "C"
-JNIEXPORT void JNICALL
-Java_com_fanhl_photofilters_PhotoFilterApi_gray(JNIEnv *env, jclass type, jobject bitmap) {
-    AndroidBitmapInfo info;
-    int ret;
-    void *pixels;
+extern "C" JNIEXPORT void JNICALL Java_com_fanhl_photofilters_PhotoFilterApi_gray(JNIEnv *env, jclass type, jobject bitmap) {
+    bitmap_hold_pixels(env, bitmap);
 
-    if ((ret = AndroidBitmap_getInfo(env, bitmap, &info)) < 0) {
-        LOGE("AndroidBitmap_getInfo() failed ! error=%d", ret);
-        return;
-    }
-    if (info.format != ANDROID_BITMAP_FORMAT_RGBA_8888) {
-        LOGE("Bitmap format is not RGBA_8888 !");
-        return;
-    }
-    if ((ret = AndroidBitmap_lockPixels(env, bitmap, &pixels)) < 0) {
-        LOGE("AndroidBitmap_lockPixels() failed ! error=%d", ret);
-    }
-
-    gray(&info, pixels);
-
-    AndroidBitmap_unlockPixels(env, bitmap);
 }
 
