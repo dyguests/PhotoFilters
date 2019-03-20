5ba7b8ec663c00fbc26da7c462a0bfddf78eace8
fanhl <fanhonglin@klicen.com>
2019-03-20 09:58:33 +0800

    upgrade

--- library/src/main/cpp/native-lib.cpp
+++ library/src/main/cpp/native-lib.cpp
@@ -91,7 +91,31 @@ void brightness(AndroidBitmapInfo *info, void *pixels, jfloat brightnessValue) {
 }
 
 void inverted(AndroidBitmapInfo *info, void *pixels) {
+    int xx, yy, red, green, blue;
+    uint32_t *line;
 
+    for (yy = 0; yy < info->height; yy++) {
+        line = (uint32_t *) pixels;
+        for (xx = 0; xx < info->width; xx++) {
+            //extract the RGB values from the pixel
+            red = (int) ((line[xx] & 0x00FF0000) >> 16);
+            green = (int) ((line[xx] & 0x0000FF00) >> 8);
+            blue = (int) (line[xx] & 0x00000FF);
+
+            //manipulate each value
+            red = rgb_clamp(255 - red);
+            green = rgb_clamp(255 - green);
+            blue = rgb_clamp(255 - blue);
+
+            // set the new pixel back in
+            line[xx] = (line[xx] & 0xFF000000)
+                       | ((red << 16) & 0x00FF0000)
+                       | ((green << 8) & 0x0000FF00)
+                       | (blue & 0x000000FF);
+        }
+
+        pixels = (char *) pixels + info->stride;
+    }
 }
 
 int rgb_clamp(int value) {
